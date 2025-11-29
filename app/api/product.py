9a4ab9cbbc458411365d973cbc0b30c6d539fe5a
ulyana <ulyanaworkpage@gmail.com>
2025-11-29 23:29:52 +0300

    feature: use_consumable_item

--- app/api/product.py
+++ app/api/product.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Depends, Header
 from app.core.dependencies import get_product_service
 from app.core.security import get_current_user
 from app.schema.product import ProductForBuy, ProductPurchaseResponse
-from app.schema.user import UserOut
+from app.schema.user import UserOut, UseConsumableItemResponse
 from app.service.product import ProductService
 
 router = APIRouter(prefix="/products", tags=["Продукты"])
@@ -26,5 +26,22 @@ async def purchase_product(
         user_id=current_user.id,
         product_id=product_id,
         payload=payload,
+        idempotency_key=idempotency_key
+    )
+
+
+@router.post(
+    "/{product_id}/use",
+    response_model=UseConsumableItemResponse
+)
+async def use_consumable_item(
+    product_id: uuid.UUID,
+    idempotency_key: str = Header(..., alias="Idempotency-Key"),
+    current_user: UserOut = Depends(get_current_user),
+    service: ProductService = Depends(get_product_service),
+):
+    return await service.use_consumable_item(
+        user_id=current_user.id,
+        product_id=product_id,
         idempotency_key=idempotency_key
     )
